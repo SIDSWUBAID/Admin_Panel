20e4fce759f77f9b9a3089499f37f08799030662
SID <SID@DESKTOP-RDHNNLL>
2023-11-15 10:43:55 -0800

    Add project files.

---
+++ Admin_Panel/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin_Panel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult setup()
+        {
+            return View();
+        }
+
+
+        public ActionResult test()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult test2()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
